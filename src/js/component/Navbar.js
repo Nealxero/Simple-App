1b5019320b07c6064462210e27724ffff8fdf996
Nealxero <santixero@gmail.com>
2023-05-02 14:27:51 +0000

    navbar finished

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -9,6 +9,9 @@ function Navbar() {
   function toggleNav() {
     setIsNavExpanded(!isNavExpanded);
   }
+  const closeMenu = () => {
+    setIsNavExpanded(false);
+  };
 
   return (
     <nav className="navigation">
@@ -21,17 +24,9 @@ function Navbar() {
           toggleNav();
         }}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="white"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
+        <svg xmlns="http://www.w3.org/2000/svg"className="h-5 w-5"viewBox="0 0 20 20"fill="white" >
+         <path fillRule="evenodd"  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+          clipRule="evenodd" /> 
         </svg>
       </button>
       <div
@@ -39,7 +34,7 @@ function Navbar() {
       >
         <ul>
           <li>
-            <a href="/home">Home</a>
+            <a href="#headerCard" onClick={closeMenu}>Home</a>
           </li>
           <li>
             <a href="/about">About</a>
